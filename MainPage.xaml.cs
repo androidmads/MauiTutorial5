72cb2a7c35355b8a1dcb6095ff2e4ea6d832c602
Mushtaq M A <mushtaqat3gb@gmail.com>
2022-10-16 18:15:17 +0530

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace MauiTutorial5;
+
+public partial class MainPage : ContentPage
+{
+    int i = 0;
+    Easing[] Easings = new Easing[]
+    {
+        Easing.Linear,
+        Easing.SinOut,
+        Easing.SinIn,
+        Easing.SinInOut,
+        Easing.CubicIn,
+        Easing.CubicOut,
+        Easing.CubicInOut,
+        Easing.BounceOut,
+        Easing.BounceIn,
+        Easing.SpringIn,
+        Easing.SpringOut,
+    };
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        await image.ScaleTo(2, 2000, Easings[i]);
+        image.Scale = 1;
+        i++;
+        if (i >= Easings.Length)
+            i = 0;
+    }
+}
+
